d48235e62d17cce22c1fbfe650ffcd8127b0c125
hongphu30102003 <163386543+hongphu30102003@users.noreply.github.com>
2024-03-21 15:58:49 +0700

    Add files via upload

--- Hồng Phú Python.py
+++ Hồng Phú Python.py
@@ -1,14 +1,18 @@
 print(len(train_indexed.columns))
+
 test_string_columns = []
 
 for col, dtype in test_df.dtypes:
     if dtype == 'string':
         test_string_columns.append(col)
-        indexers2 = [StringIndexer(inputCol=column, outputCol=column+'_index', handleInvalid='keep').fit(test_df) for column in test_string_columns]
+
+indexers2 = [StringIndexer(inputCol=column, outputCol=column+'_index', handleInvalid='keep').fit(test_df) for column in test_string_columns]
 
 pipeline2 = Pipeline(stages=indexers2)
 test_indexed = pipeline2.fit(test_df).transform(test_df)
+
 print(len(test_indexed.columns))
+
 def get_dtype(df,colname):
     return [dtype for name, dtype in df.dtypes if name == colname][0]
 
@@ -24,12 +28,15 @@ for col in test_indexed.columns:
 
 train_indexed = train_indexed.select(num_cols_train)
 test_indexed = test_indexed.select(num_cols_test)
+
 print(len(train_indexed.columns))
 print(len(test_indexed.columns))
+
 from pyspark.ml.feature import VectorAssembler
 vectorAssembler = VectorAssembler(inputCols = train_indexed.drop("SalePrice").columns, outputCol = 'features').setHandleInvalid("keep")
 
 train_vector = vectorAssembler.transform(train_indexed)
+
 vectorAssembler2 = VectorAssembler(inputCols = test_indexed.columns, outputCol = 'features').setHandleInvalid("keep")
 
 test_vector = vectorAssembler2.transform(test_indexed)
