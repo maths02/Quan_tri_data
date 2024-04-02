7a73a3b5e77f6befee01e74c74e74878b6976945
maths02 <146675131+maths02@users.noreply.github.com>
2024-04-02 22:45:13 +0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,128 @@
+#Import the library
+import tensorflow as tf
+import tensorflow_decision_forests as tfdf
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Comment this if the data visualisations doesn't work on your side
+print("TensorFlow v" + tf.__version__)
+print("TensorFlow Decision Forests v" + tfdf.__version__)
+
+#Load the dataset
+train_file_path = "D:/QTDL with Spark/train.csv"
+dataset_df = pd.read_csv(train_file_path)
+print("Full train dataset shape is {}".format(dataset_df.shape))
+dataset_df.head(3)
+#There are 79 feature columns. Using these features your model has to predict the house sale price indicated by the label column named SalePrice.
+dataset_df = dataset_df.drop('Id', axis=1)
+dataset_df.head(3)
+dataset_df.info()
+
+#House Price Distribution
+print(dataset_df['SalePrice'].describe())
+plt.figure(figsize=(9, 8))
+sns.histplot(dataset_df['SalePrice'], color='g', bins=100, hist_kws={'alpha': 0.4})
+
+#Numerical data distribution
+list(set(dataset_df.dtypes.tolist()))
+df_num = dataset_df.select_dtypes(include = ['float64', 'int64'])
+df_num.head()
+df_num.hist(figsize=(16, 20), bins=50, xlabelsize=8, ylabelsize=8)
+
+#Prepare the dataset
+
+
+def split_dataset(dataset, test_ratio=0.30):
+  test_indices = np.random.rand(len(dataset)) < test_ratio
+  return dataset[~test_indices], dataset[test_indices]
+
+train_ds_pd, valid_ds_pd = split_dataset(dataset_df)
+print("{} examples in training, {} examples in testing.".format(
+    len(train_ds_pd), len(valid_ds_pd)))
+label = 'SalePrice'
+train_ds = tfdf.keras.pd_dataframe_to_tf_dataset(train_ds_pd, label=label, task = tfdf.keras.Task.REGRESSION)
+valid_ds = tfdf.keras.pd_dataframe_to_tf_dataset(valid_ds_pd, label=label, task = tfdf.keras.Task.REGRESSION)
+#Select a Model
+tfdf.keras.get_all_models()
+rf = tfdf.keras.RandomForestModel(hyperparameter_template="benchmark_rank1", task=tfdf.keras.Task.REGRESSION)
+
+#Create a Random Forest
+rf = tfdf.keras.RandomForestModel(task = tfdf.keras.Task.REGRESSION)
+rf.compile(metrics=["mse"]) # Optional, you can use this to include a list of eval metrics
+
+#Train the model
+rf.fit(x=train_ds)
+
+#Visualize the model
+tfdf.model_plotter.plot_model_in_colab(rf, tree_idx=0, max_depth=3)
+
+#Evaluate the model on the Out of bag (OOB) data and the validation dataset
+
+logs = rf.make_inspector().training_logs()
+plt.plot([log.num_trees for log in logs], [log.evaluation.rmse for log in logs])
+plt.xlabel("Number of trees")
+plt.ylabel("RMSE (out-of-bag)")
+plt.show()
+inspector = rf.make_inspector()
+inspector.evaluation()
+evaluation = rf.evaluate(x=valid_ds,return_dict=True)
+
+for name, value in evaluation.items():
+  print(f"{name}: {value:.4f}")
+
+#Variable importances
+print(f"Available variable importances:")
+for importance in inspector.variable_importances().keys():
+  print("\t", importance)
+
+inspector.variable_importances()["NUM_AS_ROOT"]
+
+plt.figure(figsize=(12, 4))
+
+# Mean decrease in AUC of the class 1 vs the others.
+variable_importance_metric = "NUM_AS_ROOT"
+variable_importances = inspector.variable_importances()[variable_importance_metric]
+
+# Extract the feature name and importance values.
+#
+# `variable_importances` is a list of <feature, importance> tuples.
+feature_names = [vi[0].name for vi in variable_importances]
+feature_importances = [vi[1] for vi in variable_importances]
+# The feature are ordered in decreasing importance value.
+feature_ranks = range(len(feature_names))
+
+bar = plt.barh(feature_ranks, feature_importances, label=[str(x) for x in feature_ranks])
+plt.yticks(feature_ranks, feature_names)
+plt.gca().invert_yaxis()
+
+# TODO: Replace with "plt.bar_label()" when available.
+# Label each bar with values
+for importance, patch in zip(feature_importances, bar.patches):
+  plt.text(patch.get_x() + patch.get_width(), patch.get_y(), f"{importance:.4f}", va="top")
+
+plt.xlabel(variable_importance_metric)
+plt.title("NUM AS ROOT of the class 1 vs the others")
+plt.tight_layout()
+plt.show()
+
+#Submission
+test_file_path = "D:/QTDL with Spark/test.csv"
+test_data = pd.read_csv(test_file_path)
+ids = test_data.pop('Id')
+
+test_ds = tfdf.keras.pd_dataframe_to_tf_dataset(
+    test_data,
+    task = tfdf.keras.Task.REGRESSION)
+
+preds = rf.predict(test_ds)
+output = pd.DataFrame({'Id': ids,
+                       'SalePrice': preds.squeeze()})
+
+output.head()
+
+ample_submission_df = pd.read_csv('../input/house-prices-advanced-regression-techniques/sample_submission.csv')
+sample_submission_df['SalePrice'] = rf.predict(test_ds)
+sample_submission_df.to_csv('/kaggle/working/submission.csv', index=False)
+sample_submission_df.head()
+
